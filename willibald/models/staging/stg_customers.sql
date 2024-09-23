bc0c87a8021e853563fdd715caccb2a9ef24d59c
Joscha von Hein <joscha.vonhein@cimt-ag.de>
2024-09-23 13:06:55 +0200

    Add customer model, test, docs

---
+++ willibald/models/staging/stg_customers.sql
@@ -0,0 +1,5 @@
+select
+    kundeid as customer_id
+    , vorname as first_name
+    , name as last_name
+from willibald_shop_p1.kunde
---
+++ willibald/models/staging/stg_orders.sql
@@ -0,0 +1,6 @@
+select 
+    bestellungid as order_id
+    , kundeid as customer_id
+    , bestelldatum as order_date
+    , wunschdatum
+from willibald_shop_p1.bestellung
